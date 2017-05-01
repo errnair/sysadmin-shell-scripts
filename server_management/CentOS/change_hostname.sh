3a26f0c63c39ab2502123798d59073a85c5fd8c1
rn4ir <shell_scripts@devserver.local.vm>
2017-05-01 09:46:27 -0400

    Change hostname script added - CentOS

---
+++ server_management/CentOS/change_hostname.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#################################################
+#						#
+#     A shell script to change the server's	#
+#     hostname on CentOS 7. Uses 'hostnamectl'.	#
+#						#
+#################################################
+
+
+new_hostname=""
+
+# check if the current user is root
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+    echo -e "This looks like a 'non-root' user.\nPlease switch to 'root' and run the script again."
+    exit
+fi
+
+# get user input for the new hostname
+if [ "$1" != "" ]; then
+    new_hostname=$1
+else
+    echo -e "Usage: ./change_hostname.sh <new hostname>\nPlease run the script once more WITH the new hostname."
+    exit
+fi
+
+# detect original hostname
+old_hostname=$(/usr/bin/hostnamectl | grep hostname | cut -d: -f2)
+
+# change hostname
+/usr/bin/hostnamectl set-hostname $new_hostname
+/usr/bin/systemctl restart network
+/usr/bin/systemctl restart NetworkManager
+
+# Print completion message
+echo -e "\nHostname has been changed FROM $old_hostname TO $new_hostname.\nYou may need to log off and log back in from the shell session to see the changes reflected."
