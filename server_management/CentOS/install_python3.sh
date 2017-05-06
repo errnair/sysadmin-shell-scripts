2113020a7a8de68bfaa0e5a298ccbc98d06c5bb9
rn4ir <rn@rahul-nair.com>
2017-05-06 02:26:55 -0400

    Added comments for the Python install script

--- server_management/CentOS/install_python3.sh
+++ server_management/CentOS/install_python3.sh
@@ -13,27 +13,33 @@ else
 	yum install gcc -y > /dev/null 2>&1
 fi
 
+# Install packages required to build Python from source
 echo -e "\nInstalling developement tools required to build from source."
 yum groupinstall "Development Tools" -y > /dev/null 2>&1
 yum install zlib-devel -y > /dev/null 2>&1
 
+# To download Python 3's source from the official repository
 echo -e "\nDownloading Python source..."
 mkdir -p /opt/src/python3 > /dev/null 2>&1
 cd /opt/src/python3/ > /dev/null 2>&1
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz > /dev/null 2>&1
 
+# Extract the Python source
 echo -e "\nUnzipping Python source..."
 tar -xf Python-3.6.1.tar.xz > /dev/null 2>&1
 cd Python-3.6.1 > /dev/null 2>&1
 mkdir /usr/bin/python36/
 
+# Compile and build Python3 from the extracted source
 echo -e "\nCompiling the Python source..."
 ./configure --prefix=/usr/bin/python36/ > /dev/null 2>&1
 echo -e "\nBuilding from source..."
 make > /dev/null 2>&1
 make install > /dev/null 2>&1
 
+# Create symlinks so that all the users on the system can use Python3
 echo -e "\nBuild complete. Creating symlinks."
 ln -s /usr/bin/python36/bin/python3.6 /usr/bin/python3
 
+# Report status
 echo -e "\n\nAll done.\nCheck the commands 'which python3' and 'python3 -V'\n"
