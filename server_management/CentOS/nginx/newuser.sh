6f2e4b03269a4b49a1b7a803eca78623345abc73
rahul <rn@rahul-nair.com>
2017-07-04 13:28:54 -0400

    changed contexts for homedir

--- server_management/CentOS/nginx/newuser.sh
+++ server_management/CentOS/nginx/newuser.sh
@@ -21,4 +21,14 @@ else
     exit
 fi
 
+echo "\nCreating new user and adding the user to the 'wheel' group"
+useradd $newuser
+usermod -aG wheel $newuser
+
+echo "\nCreating webroot and log locations, and assigning required permissions"
+mkdir /home/$newuser/public_html
+mkdir /home/$newuser/logs
+chcon -Rt httpd_log_t /home/$newuser/logs/
+chcon -Rt httpd_sys_content_t /home/$newuser/public_html/
+
 
