95b7afcf6c2d64fcc09a3532e6287ab58033a14a
devuser <devuser@dev.machine>
2017-07-04 13:06:55 -0400

    added Nginx newuser script

---
+++ server_management/CentOS/nginx/newuser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#################################################
+#                                               #
+#     A shell script create a new user - Nginx. #
+#                                               #
+#################################################
+
+newuser=""
+
+# check if the current user is root
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+    echo -e "This looks like a 'non-root' user.\nPlease switch to 'root' and run the script again."
+    exit
+fi
+
+# get user input for the new hostname
+if [ "$1" != "" ]; then
+    newuser=$1
+else
+    echo -e "Usage: ./newuser.sh <new username>\nPlease run the script once more WITH the new username."
+    exit
+fi
+
+
