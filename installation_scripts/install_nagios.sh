14119cb727e7a61fd177360b918a2e7932078488
rahul <rn@rahul-nair.com>
2017-08-02 20:36:47 -0400

    added nagios plugin and nrpe installation

--- installation_scripts/install_nagios.sh
+++ installation_scripts/install_nagios.sh
@@ -19,7 +19,7 @@ preresquisites() {
     groupadd nagcmd
     useradd -G nagcmd nagios
 
-    yum install httpd mod_ssl
+    yum install httpd mod_ssl -y
     systemctl enable httpd
     systemctl start httpd
     firewall-cmd --permanent --add-service=http
@@ -40,9 +40,45 @@ install_nagios(){
     make install-commandmode
     make install-config
     make install-webconf
-    usermod -aG nagcmd apache
+    echo -e "\n\nNagios Directory Location: /usr/local/nagios/"
 
+    usermod -aG nagcmd apache
 }
 
 install_nagios_plugins() {
+    cd /opt/sources/nagios
+    mkdir nagios-plugins
+    cd nagios-plugins/
+    wget http://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
+    tar zxf nagios-plugins-*.tar.gz
+    cd nagios-plugins-*
+    ./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
+    make
+    make install
+}
+
+install_nrpe() {
+    cd /opt/sources/nagios
+    mkdir nrpe
+    cd nrpe
+    wget https://downloads.sourceforge.net/project/nagios/nrpe-3.x/nrpe-3.1.0.tar.gz
+    tar zxf nrpe-*.tar.gz
+    cd nrpe-*
+    ./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib64/
+    make all
+    make install
+    make install-config
+
+    echo -e "nrpe\t\t5666/tcp\t\t# Nagios NRPE" >> /etc/services
+
+    make install-init
+
+    systemctl enable nrpe.service
+    firewall-cmd --zone=public --add-port=5666/tcp
+    firewall-cmd --zone=public --add-port=5666/tcp --permanent
+    firewall-cmd --reload
+
+    sed -i.bak 's/^\(dont_blame_nrpe=\).*/\11/' /usr/local/nagios/etc/nrpe.cfg
+    ipaddr=$(hostname -I | cut -d" " -f 1)
+
 }
