89383bc0e4978b3850c639542bb8ef6c4bfec81d
rahul <rn@rahul-nair.com>
2017-08-09 16:37:04 -0400

    added nginx installation skeleton

---
+++ installation_scripts/install_nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#################################################
+#                                               #
+# A shell script to install Nagios on CentOS    #
+#                                               #
+#################################################
+
+# check if the current user is root
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+    echo -e "This looks like a 'non-root' user.\nPlease switch to 'root' and run the script again."
+    exit
+fi
+
