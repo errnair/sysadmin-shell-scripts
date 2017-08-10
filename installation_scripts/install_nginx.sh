3b4a0a9af233a877b3b504d279ac80c7b6e06996
rahul <rn@rahul-nair.com>
2017-08-09 21:50:48 -0400

    added nginx installation section

--- installation_scripts/install_nginx.sh
+++ installation_scripts/install_nginx.sh
@@ -11,3 +11,21 @@ if [[ $(/usr/bin/id -u) != "0" ]]; then
     exit
 fi
 
+domain=$1
+
+if [[ $domain == '' ]]; then
+    echo -e "Usage: ./install_nginx domain-name.tld"
+    exit
+fi
+
+install_nginx() {
+    yum update -y
+    echo -e "[nginx]\nname=nginx repo\nbaseurl=http://nginx.org/packages/rhel/7/\$basearch/\ngpgcheck=0\nenabled=1" >> /etc/yum.repos.d/nginx.repo
+    yum install nginx -y
+    systemctl enable nginx
+    systemctl start nginx
+    firewall-cmd --zone=public --add-service=http --permanent
+    firewall-cmd --zone=public --add-service=https --permanent
+    firewall-cmd --reload
+    systemctl restart nginx
+}
