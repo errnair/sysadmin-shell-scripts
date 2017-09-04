000951edde4fb1a672133746f9849653bd93a04a
rahul <rn@rahul-nair.com>
2017-09-04 00:16:39 -0400

    added comments

--- server_management/Debian/install_salt_minion.sh
+++ server_management/Debian/install_salt_minion.sh
@@ -12,6 +12,7 @@ if [[ $(/usr/bin/id -u) != "0" ]]; then
     exit
 fi
 
+# Check if user-input exists
 if [ "$1" != "" ]; then
     new_hostname=$1
 else
@@ -21,19 +22,29 @@ fi
 
 MASTERIP=$1
 
+# Function to install salt-minion
 install_minion() {
+
+    # Get the saltstack key and create the salt repo
     wget -O - https://repo.saltstack.com/apt/debian/8/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
     echo -e "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" >> /etc/apt/sources.list.d/saltstack.list
+
     apt-get update
+
+    # Install salt-minion and firewalld
     apt-get install salt-minion salt-ssh firewalld -y
     
+    # Open the Salt-minon port
     firewall-cmd --zone=public --add-port=4506/tcp --permanent
     firewall-cmd --reload
 
+    # Add the Salt-Master's IP to the minion's config file
     sed -i "s/\#master:\ salt/master:\ $MASTERIP/g" /etc/salt/minion
     
+    # Enable and start the minion service
     systemctl enable salt-minion
     systemctl start salt-minion
 }
 
+# Calls the install_minion function
 install_minion
