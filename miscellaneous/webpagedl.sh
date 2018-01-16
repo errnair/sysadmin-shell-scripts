1d58fdecd9aa9f4f95f4c918c54f9cf3e1cf4a80
rahul <rn@rahul-nair.com>
2018-01-15 20:48:15 -0500

    script to download a webpage

---
+++ miscellaneous/webpagedl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##########################################################
+# Script to download a webpage using the 'wget' command  #
+# Usage: ./webpagedl.sh <full webpage URL>               #
+##########################################################
+
+pagelink=$1
+if [ -z "$pagelink" ]; then
+    echo "usage: ./webpagedl.sh <full webpage URL>"
+    exit 1
+fi
+
+mkdir -p webpages
+dldir="webpages/webpage.link"
+wget -q -O "$dldir" "$pagelink"
+get_exitcode=$?
+if [ $get_exitcode -ne 0 ]; then
+    echo "ERROR: webpagedl: the link '$pagelink' does not return a document (wget exit code $get_exitcode)" 1>&2
+    exit 2
+fi
+echo "$dldir"
